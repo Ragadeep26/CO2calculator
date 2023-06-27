bbad34796e4e48bed6dd414be5ebbac138747568
Ragadeep26 <ragadeep14026@gmail.com>
2023-06-27 15:14:42 +0200

    letssee

--- src/main_co2_calculator_EPD.py
+++ src/main_co2_calculator_EPD.py
@@ -241,6 +241,7 @@ def continue_program(st, parameters):
                     elif isinstance(structure, MIPWall):
                         if st.checkbox('With  Steel structures'):
                             Steelstructure = 1
+                            isinstance(structure, MIPSteelProfileWall)
                             cols = tab.columns(3)
                             structure.wall_area = cols[0].number_input('Wall area [m^2]', value=structure.wall_area, step=100.0, help='Area of the constructed wall', key='wall_area_MIPSteelwall'+str(i))
                             structure.wall_thickness = cols[1].number_input('Wall thickness [m]', value=structure.wall_thickness, step=0.1, key='wall_thickness_MIPSteelwall'+str(i))
@@ -297,6 +298,7 @@ def continue_program(st, parameters):
                     elif isinstance(structure, MIPWall_EPD):
                         tab.header('Details for MIP as cut-off wall according to EPD')
                         if st.checkbox('With  Steel structures'):
+                            isinstance(structure, MIPSteelProfileWall_EPD)
                             Steelstructure = 1
                             cols = tab.columns(3)
                             structure.wall_area = cols[0].number_input('Wall area [m^2]', value=structure.wall_area, step=100.0, help='Area of the constructed wall', key='wall_area_MIPSteelwall'+str(i))
