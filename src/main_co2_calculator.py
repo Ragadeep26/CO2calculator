be747b16a8c58fa2ed282bca658e62c1e4ec95c3
Ragadeep26 <ragadeep14026@gmail.com>
2023-06-27 17:01:33 +0200

    mehhh

--- src/main_co2_calculator.py
+++ src/main_co2_calculator.py
@@ -240,6 +240,7 @@ def continue_program(st, parameters):
                         tab.header('Details for MIP as cut-off wall')
                         if st.checkbox('With  Steel structures'):
                             cols = tab.columns(3)
+                            structure = MIPSteelProfileWall()
                             structure.wall_area = cols[0].number_input('Wall area [m^2]', value=structure.wall_area, step=100.0, help='Area of the constructed wall', key='wall_area_MIPSteelwall'+str(i))
                             structure.wall_thickness = cols[1].number_input('Wall thickness [m]', value=structure.wall_thickness, step=0.1, key='wall_thickness_MIPSteelwall'+str(i))
                             structure.productivity = cols[2].number_input('Production rate (productivity) [m^2/working day]', value=structure.productivity, step=10.0, help='Basic value (without obstruction): 170 m^2/working day', key='productivity_MIPSteelwall'+str(i))
