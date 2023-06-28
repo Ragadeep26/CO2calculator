df5d5ac79bbf149072571dc9744e2bb990b9ff8c
Ragadeep26 <ragadeep14026@gmail.com>
2023-06-28 09:07:31 +0200

    TO main hopefully

--- src/co2_calculator/structures/wall_MIP_steelprofile_EPD.py
+++ src/co2_calculator/structures/wall_MIP_steelprofile_EPD.py
@@ -10,8 +10,8 @@ filename_database = './src/co2_calculator/documents/CO2-eq Fußabdruck - Verglei
 class MIPSteelProfileWall_EPD(BaseWall_EPD):
     """ Concrete class for pile walls"""
     def __init__(self, wall_area=3430.0, wall_thickness=0.55, classification='Class I', weight_steelprofile=270.0,
-                unit_co2_A1_to_A3={'Class I': 37.3, 'Class II': 37.3, 'Class III': 37.3, 'Class IV': 37.3, 'Class V': 37.3, 'Class VI': 37.3}, 
-                unit_co2_A5={'Class I': 20.6, 'Class II': 20.6, 'Class III': 20.6, 'Class IV': 20.6, 'Class V': 20.6, 'Class VI': 20.6}):
+                unit_co2_A1_to_A3={'Class I': 37.3, 'Class II': 58.7, 'Class III': 84.9, 'Class IV': 127.5, 'Class V': 186.2, 'Class VI': 235.2}, 
+                unit_co2_A5={'Class I': 20.6, 'Class II': 23.3, 'Class III': 23.3, 'Class IV': 23.3, 'Class V': 23.3, 'Class VI': 23.3}):
         super().__init__()
         self.classification = classification                # class 1, cement weight from 60 kg/m^3 to 100 kg/m^3
         self.unit_co2_A1_to_A3 = unit_co2_A1_to_A3          # kg CO2_eq per m^3
